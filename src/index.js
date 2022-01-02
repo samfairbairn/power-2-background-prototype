e76a2d8d20a27f3738693acc2c326d8335379632
sam <samfairbairn@hotmail.co.uk>
2022-01-02 19:36:53 +0800

    import fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/common.scss';
+import './styles/fonts.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
