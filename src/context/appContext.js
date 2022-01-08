41df499813267130ea150a1177e9a407d016a6e6
sam <samfairbairn@hotmail.co.uk>
2022-01-08 15:12:38 +0800

    save scroll settings in context

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -5,9 +5,11 @@ export const AppContext = createContext({});
 const AppProvider = ({ children }) => {
   const [lightMode, setLightMode] = useState(false);
   const [scrollPos, setScrollPos] = useState(0);
+  const [scroll, setScroll] = useState(undefined);
+
   
   return (
-    <AppContext.Provider value={{lightMode, setLightMode, scrollPos, setScrollPos}}>
+    <AppContext.Provider value={{lightMode, setLightMode, scrollPos, setScrollPos, setScroll, scroll}}>
       {children}
     </AppContext.Provider>
   );
