08d89ed2270bf31d5bc0d1086099334ccca49e7d
sam <samfairbairn@hotmail.co.uk>
2022-01-13 17:42:54 +0800

    context mobile/size watchers

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect, useRef } from "react";
 
 export const AppContext = createContext({});
 
@@ -6,7 +6,28 @@ const AppProvider = ({ children }) => {
   const [lightMode, setLightMode] = useState(false);
   const [scrollPos, setScrollPos] = useState(0);
   const [scroll, setScroll] = useState(undefined);
+  const [screenRatio, setScreenRatio] = useState(undefined);
+  const [isMobile, setIsMobile] = useState(undefined);
 
+  let resizeTimer = useRef()
+
+  const onResize = (e) => {
+    if (resizeTimer.current) {
+      clearTimeout(resizeTimer.current)
+      resizeTimer.current = undefined
+    }
+    resizeTimer.current = setTimeout(() => {
+      clearTimeout(resizeTimer.current)
+      resizeTimer.current = undefined
+      setScreenRatio(window.innerWidth / window.innerHeight)
+    }, 500)
+  }
+
+  useEffect(() => {
+    setScreenRatio(window.innerWidth / window.innerHeight)
+    setIsMobile(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+    window.addEventListener('resize', onResize)
+  }, [])
   
   return (
     <AppContext.Provider value={{lightMode, setLightMode, scrollPos, setScrollPos, setScroll, scroll}}>
