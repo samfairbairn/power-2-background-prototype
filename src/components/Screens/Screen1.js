b00c4a9d564127a660fe44b73e3cfb45d44d20dc
sam <samfairbairn@hotmail.co.uk>
2022-01-15 11:49:28 +0800

    use timeout to show page1 title

--- src/components/Screens/Screen1.js
+++ src/components/Screens/Screen1.js
@@ -1,15 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import _styles from './screen.module.scss';
 import styles from './screen1.module.scss';
 import classNames from 'classnames';
-import useFontFaceObserver from 'use-font-face-observer'
 
 const Screen1 = () => {
-  
-  const webFontsLoaded = useFontFaceObserver([{family: `Brandon Grotesque`, weight: `bold`,}]);
+
+  const [showTitle, setShowTitle] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowTitle(true)
+    }, 750)
+  }, [])
 
   return (
     <div className={styles.screen} style={{top: `0vh`}}>
-      <div className={classNames(styles.content, webFontsLoaded && styles.show)}>
+      <div className={classNames(styles.content, showTitle && styles.show)}>
         <h1>
           <span className={styles.titleLine}>POWER2 No-Loss Cube</span><br />
           <span className={classNames([styles.titleLine, styles.mobileTitle])}>Win <span className={_styles.gradient1}>1</span> + <span className={_styles.gradient2}>1</span> Prizes</span><br />
