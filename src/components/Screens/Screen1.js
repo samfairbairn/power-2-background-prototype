9bce1ff2d173a116341ed15bef529f7491f7c43d
sam <samfairbairn@hotmail.co.uk>
2022-01-21 14:39:42 +0800

    mobile title screen text

--- src/components/Screens/Screen1.js
+++ src/components/Screens/Screen1.js
@@ -16,10 +16,17 @@ const Screen1 = () => {
   return (
     <div className={styles.screen} style={{top: `0vh`}}>
       <div className={classNames(styles.content, showTitle && styles.show)}>
-        <h1>
-          <span className={styles.titleLine}>POWER2 <br className="mobile-only" />No-Loss Cube</span><br />
-          <span className={classNames([styles.titleLine, styles.mobileTitle])}>Win <span className={_styles.gradient1}>1</span> + <span className={_styles.gradient2}>1</span> Prizes</span><br />
-          <span className={classNames([styles.titleLine, styles.mobileTitle])}>Every single day</span>
+        <h1 className="desktop-only">
+          <span className={styles.titleLine}>POWER2 No-Loss Cube</span><br />
+          <span className={styles.titleLine}>Win <span className={_styles.gradient1}>1</span> + <span className={_styles.gradient2}>1</span> Prizes</span><br />
+          <span className={styles.titleLine}>Every single day</span>
+        </h1>
+        <h1 className='mobile-only'>
+          <span style={{marginBottom: '20px', display: 'inline-block'}}>POWER2 <br />No-Loss Cube</span><br />
+          Win <span className={_styles.gradient1}>1</span> + <span className={_styles.gradient2}>1</span> Prizes<br />
+          Every<br />
+          single<br />
+          day
         </h1>
       </div>
     </div>
